e748c0d56e53dfc09055c2957dbb7ad3ac994789
IvaBoo <urijsavelev314@gmail.com>
2025-10-08 00:00:02 +0000

    Refactor helper functions (2025-10-08 00:00:02.466124)

---
+++ config_loader.py
@@ -0,0 +1,8 @@
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption("--host", "-H", action="store", default=None)
+
+
+def fire(self, *, reverse=False, **kwargs):
+        pass
+
