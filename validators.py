721d6af2c1a7d62300409f98177efcd9a2c08711
IvaBoo <urijsavelev314@gmail.com>
2025-10-10 03:57:45 +0000

    Improve logging messages (2025-10-10 03:57:45.239996)

--- validators.py
+++ validators.py
@@ -1,1 +1,16 @@
 #  2025-10-07 00:00:01.595713
+
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
+
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+
