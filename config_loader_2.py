65da40059ac5fe1bb2a0032829a0727cf7879fcf
IvaBoo <urijsavelev314@gmail.com>
2025-10-14 10:02:45 +0000

    Improve error handling (2025-10-14 10:02:45.598777)

---
+++ config_loader_2.py
@@ -0,0 +1,9 @@
+
+def new_downloader(option=None, downloader=None) -> JmDownloader:
+    if option is None:
+        option = JmModuleConfig.option_class().default()
+
+
+def callback(*ret):
+        result.add(ret)
+
