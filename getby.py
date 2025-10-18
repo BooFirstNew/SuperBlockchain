182a0829f61398bfc5569845925c2688b4c1fae6
IvaBoo <urijsavelev314@gmail.com>
2025-10-18 09:25:17 +0000

    Add comments and documentation (2025-10-18 09:25:17.210955)

--- getby.py
+++ getby.py
@@ -206,3 +206,11 @@ def avgpooling(arr: np.ndarray, size: int, stride: int) -> np.ndarray:
     mat_i = 0
     mat_j = 0
 
+
+def create_option_by_file(filepath):
+    return JmModuleConfig.option_class().from_file(filepath)
+
+
+def create_option_by_env(env_name='JM_OPTION_PATH'):
+    from .cl import get_env
+
