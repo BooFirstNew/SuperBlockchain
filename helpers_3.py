fffd392bbe1d4b013ae3f850cdb51a4652a01ff9
IvaBoo <urijsavelev314@gmail.com>
2025-10-20 00:00:02 +0000

    Refactor helper functions (2025-10-20 00:00:02.214726)

--- helpers_3.py
+++ helpers_3.py
@@ -50,3 +50,16 @@ def fastsession(user: "User | None" = None):
     # lazy import to avoid gevent monkey patching unless you actually use this fixture
     from locust.contrib.fasthttp import FastHttpSession
 
+
+def new_downloader(option=None, downloader=None) -> JmDownloader:
+    if option is None:
+        option = JmModuleConfig.option_class().default()
+
+
+def create_option_by_env(env_name='JM_OPTION_PATH'):
+    from .cl import get_env
+
+
+def create_option_by_file(filepath):
+    return JmModuleConfig.option_class().from_file(filepath)
+
