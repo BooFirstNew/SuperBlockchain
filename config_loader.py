c12bcca39a60fc9bdde408c07df399141a28f17b
IvaBoo <urijsavelev314@gmail.com>
2025-10-26 01:12:09 +0000

    Update dependencies (2025-10-26 01:12:09.569966)

--- config_loader.py
+++ config_loader.py
@@ -43,3 +43,14 @@ def main() -> None:
 def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption("--host", "-H", action="store", default=None)
 
+
+def callback(*ret):
+        result.add(ret)
+
+
+def create_option_by_str(text: str, mode=None):
+    if mode is None:
+        mode = PackerUtil.mode_yml
+    data = PackerUtil.unpack_by_str(text, mode)[0]
+    return JmModuleConfig.option_class().construct(data)
+
