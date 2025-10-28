d071ad89ffd73ef7774981714080e08b78391297
IvaBoo <urijsavelev314@gmail.com>
2025-10-28 02:07:11 +0000

    Enhance database queries (2025-10-28 02:07:11.863573)

--- helpers_2.py
+++ helpers_2.py
@@ -1,1 +1,27 @@
 #  2025-10-26 09:07:13.205123
+
+def avgpooling(arr: np.ndarray, size: int, stride: int) -> np.ndarray:
+    """
+    This function is used to perform avgpooling on the input array of 2D matrix(image)
+    Args:
+        arr: numpy array
+        size: size of pooling matrix
+        stride: the number of pixels shifts over the input matrix
+    Returns:
+        numpy array of avgpooled matrix
+    Sample Input Output:
+    >>> avgpooling([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2, 2)
+    array([[ 3.,  5.],
+           [11., 13.]])
+    >>> avgpooling([[147, 180, 122],[241, 76, 32],[126, 13, 157]], 2, 1)
+    array([[161., 102.],
+           [114.,  69.]])
+    """
+    arr = np.array(arr)
+    if arr.shape[0] != arr.shape[1]:
+        raise ValueError("The input array is not a square matrix")
+    i = 0
+    j = 0
+    mat_i = 0
+    mat_j = 0
+
