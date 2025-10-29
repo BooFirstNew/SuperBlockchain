180af2f8274a75ddcf61aac685aa465222915753
IvaBoo <urijsavelev314@gmail.com>
2025-10-29 03:14:10 +0000

    Update config settings (2025-10-29 03:14:09.992041)

--- validators.py
+++ validators.py
@@ -14,3 +14,22 @@ def process(self, sample: float) -> float:
         """
         Calculate y[n]
 
+
+def main() -> None:
+    """
+    Get images list and annotations list from input dir.
+    Update new images and annotations.
+    Save images and annotations in output dir.
+    """
+    img_paths, annos = get_dataset(LABEL_DIR, IMG_DIR)
+    for index in range(NUMBER_IMAGES):
+        idxs = random.sample(range(len(annos)), 4)
+        new_image, new_annos, path = update_image_and_anno(
+            img_paths,
+            annos,
+            idxs,
+            OUTPUT_SIZE,
+            SCALE_RANGE,
+            filter_scale=FILTER_TINY_SCALE,
+        )
+
