e533835ffe69880e3e1f7999a6f83a17d650de0e
IvaBoo <urijsavelev314@gmail.com>
2025-11-06 00:00:01 +0000

    Fix bug in data processing (2025-11-06 00:00:01.955739)

--- services_2.py
+++ services_2.py
@@ -3,3 +3,18 @@ def process(self, sample: float) -> float:
         """
         Calculate y[n]
 
+
+def get_dataset(label_dir: str, img_dir: str) -> tuple[list, list]:
+    """
+    - label_dir <type: str>: Path to label include annotation of images
+    - img_dir <type: str>: Path to folder contain images
+    Return <type: list>: List of images path and labels
+    """
+    img_paths = []
+    labels = []
+    for label_file in glob.glob(os.path.join(label_dir, "*.txt")):
+        label_name = label_file.split(os.sep)[-1].rsplit(".", 1)[0]
+        with open(label_file) as in_file:
+            obj_lists = in_file.readlines()
+        img_path = os.path.join(img_dir, f"{label_name}.jpg")
+
