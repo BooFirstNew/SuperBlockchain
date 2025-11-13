a2ddf7bb4b9dc28a8aae56cd19f6eaa2de71bcec
IvaBoo <urijsavelev314@gmail.com>
2025-11-13 00:00:03 +0000

    Enhance database queries (2025-11-13 00:00:03.088384)

--- utils.py
+++ utils.py
@@ -1,1 +1,35 @@
 #  2025-11-06 07:51:22.731993
+
+def main() -> None:
+    """
+    Get images list and annotations list from input dir.
+    Update new images and annotations.
+    Save images and annotations in output dir.
+    """
+    img_paths, annos = get_dataset(LABEL_DIR, IMG_DIR)
+    for index in range(NUMBER_IMAGES):
+        idxs = random.sample(range(len(annos)), 4)
+        new_image, new_annos, path = update_image_and_anno(
+            img_paths,
+            annos,
+            idxs,
+            OUTPUT_SIZE,
+            SCALE_RANGE,
+            filter_scale=FILTER_TINY_SCALE,
+        )
+
+
+def get_dataset(label_dir: str, img_dir: str) -> tuple[list, list]:
+    """
+    - label_dir <type: str>: Path to label include annotation of images
+    - img_dir <type: str>: Path to folder contain images
+    Return <type: list>: List of images path and labels
+    """
+    img_paths = []
+    labels = []
+    for label_file in glob.glob(os.path.join(label_dir, "*.txt")):
+        label_name = label_file.split(os.sep)[-1].rsplit(".", 1)[0]
+        with open(label_file) as in_file:
+            obj_lists = in_file.readlines()
+        img_path = os.path.join(img_dir, f"{label_name}.jpg")
+
