71fd6773c9d2ac3b3dad8720a2f048daeaad26bf
IvaBoo <urijsavelev314@gmail.com>
2025-11-15 00:00:02 +0000

    Update README and documentation (2025-11-15 00:00:02.045718)

---
+++ auth.py
@@ -0,0 +1,10 @@
+
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
