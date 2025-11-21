c087571117328c2999cc00ac4abe7be1ad6f1e7a
IvaBoo <urijsavelev314@gmail.com>
2025-11-21 07:21:19 +0000

    gogo (2025-11-21 07:21:19.239973)

--- scheduler.py
+++ scheduler.py
@@ -116,3 +116,18 @@ def create_option_by_file(filepath):
 def create_option_by_env(env_name='JM_OPTION_PATH'):
     from .cl import get_env
 
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
+
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+
+
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+
