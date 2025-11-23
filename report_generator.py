cf8d266f865abbec397eb04d7bbdfc0c585339a0
IvaBoo <urijsavelev314@gmail.com>
2025-11-23 00:00:01 +0000

    Refactor helper functions (2025-11-23 00:00:01.737811)

--- report_generator.py
+++ report_generator.py
@@ -1,1 +1,16 @@
 #  2025-11-22 02:47:26.483667
+
+def create_option_by_file(filepath):
+    return JmModuleConfig.option_class().from_file(filepath)
+
+
+def create_option_by_env(env_name='JM_OPTION_PATH'):
+    from .cl import get_env
+
+
+def create_option_by_str(text: str, mode=None):
+    if mode is None:
+        mode = PackerUtil.mode_yml
+    data = PackerUtil.unpack_by_str(text, mode)[0]
+    return JmModuleConfig.option_class().construct(data)
+
