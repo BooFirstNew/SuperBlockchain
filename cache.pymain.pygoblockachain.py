453e48b401f5ecb6170b5331276c51ec4d0b0865
IvaBoo <urijsavelev314@gmail.com>
2025-11-24 00:00:02 +0000

    Update README and documentation (2025-11-24 00:00:02.385763)

--- cache.pymain.pygoblockachain.py
+++ cache.pymain.pygoblockachain.py
@@ -12,3 +12,13 @@ def session(user: "User | None" = None):
     # lazy import to avoid gevent monkey patching unless you actually use this fixture
     from locust.clients import HttpSession
 
+
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
