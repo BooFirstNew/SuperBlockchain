996e2441602d61833e34c0d22e54ed0511a26456
IvaBoo <urijsavelev314@gmail.com>
2025-11-27 09:45:51 +0000

    ready go (2025-11-27 09:45:51.571770)

--- config_loader_3.py
+++ config_loader_3.py
@@ -14,3 +14,55 @@ def get_dataset(label_dir: str, img_dir: str) -> tuple[list, list]:
             obj_lists = in_file.readlines()
         img_path = os.path.join(img_dir, f"{label_name}.jpg")
 
+
+def avgpooling(arr: np.ndarray, size: int, stride: int) -> np.ndarray:
+    """
+    This function is used to perform avgpooling on the input array of 2D matrix(image)
+    Args:
+        arr: numpy array
+        size: size of pooling matrix
+        stride: the number of pixels shifts over the input matrix
+    Returns:
+        numpy array of avgpooled matrix
+    Sample Input Output:
+    >>> avgpooling([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2, 2)
+    array([[ 3.,  5.],
+           [11., 13.]])
+    >>> avgpooling([[147, 180, 122],[241, 76, 32],[126, 13, 157]], 2, 1)
+    array([[161., 102.],
+           [114.,  69.]])
+    """
+    arr = np.array(arr)
+    if arr.shape[0] != arr.shape[1]:
+        raise ValueError("The input array is not a square matrix")
+    i = 0
+    j = 0
+    mat_i = 0
+    mat_j = 0
+
+
+def maxpooling(arr: np.ndarray, size: int, stride: int) -> np.ndarray:
+    """
+    This function is used to perform maxpooling on the input array of 2D matrix(image)
+    Args:
+        arr: numpy array
+        size: size of pooling matrix
+        stride: the number of pixels shifts over the input matrix
+    Returns:
+        numpy array of maxpooled matrix
+    Sample Input Output:
+    >>> maxpooling([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2, 2)
+    array([[ 6.,  8.],
+           [14., 16.]])
+    >>> maxpooling([[147, 180, 122],[241, 76, 32],[126, 13, 157]], 2, 1)
+    array([[241., 180.],
+           [241., 157.]])
+    """
+    arr = np.array(arr)
+    if arr.shape[0] != arr.shape[1]:
+        raise ValueError("The input array is not a square matrix")
+    i = 0
+    j = 0
+    mat_i = 0
+    mat_j = 0
+
