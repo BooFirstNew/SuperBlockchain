3448bef3df18caa0c1f28a3b80118f3c295b6090
IvaBoo <urijsavelev314@gmail.com>
2025-12-09 00:00:03 +0000

    Update config settings (2025-12-09 00:00:03.083061)

--- data_processing.py
+++ data_processing.py
@@ -9,3 +9,15 @@ def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
     Show phase response of a filter
 
 #  2025-12-03 08:11:49.791067
+
+def random_chars(number_char: int) -> str:
+    """
+    Automatic generate random 32 characters.
+    Get random string code: '7b7ad245cdff75241935e4dd860f3bad'
+    >>> len(random_chars(32))
+    32
+    """
+    assert number_char > 1, "The number of character should greater than 1"
+    letter_code = ascii_lowercase + digits
+    return "".join(random.choice(letter_code) for _ in range(number_char))
+
