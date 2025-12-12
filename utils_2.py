11873f62b9e3f0ef989646f8df5d10f3a69aa27a
IvaBoo <urijsavelev314@gmail.com>
2025-12-12 04:56:57 +0000

    Add caching mechanism (2025-12-12 04:56:57.578696)

--- utils_2.py
+++ utils_2.py
@@ -1,1 +1,11 @@
 #  2025-11-22 05:32:52.569208
+
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
