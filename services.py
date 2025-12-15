6517fd5aa72a21c103652334dca075d2bfe0f18c
IvaBoo <urijsavelev314@gmail.com>
2025-12-15 01:29:27 +0000

    Fix bug in data processing (2025-12-15 01:29:27.978302)

--- services.py
+++ services.py
@@ -13,3 +13,8 @@ def create_option_by_file(filepath):
 #  2025-11-18 03:30:42.726360
 #  2025-12-02 05:46:08.079556
 #  2025-12-13 00:00:01.481982
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
