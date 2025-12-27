cb10edf67b694932e13cef7e0f2953e09e32e8a6
IvaBoo <urijsavelev314@gmail.com>
2025-12-27 06:23:00 +0000

    Refactor user authentication (2025-12-27 06:23:00.741122)

--- utils_2.py
+++ utils_2.py
@@ -55,3 +55,13 @@ def main() -> None:
             filter_scale=FILTER_TINY_SCALE,
         )
 
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
+
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+
