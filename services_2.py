333bac01c98109bf50fc6fb9da566f572079f467
IvaBoo <urijsavelev314@gmail.com>
2025-12-31 00:00:02 +0000

    Improve logging messages (2025-12-31 00:00:02.168252)

--- services_2.py
+++ services_2.py
@@ -39,3 +39,8 @@ def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
     """
     Show phase response of a filter
 
+
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+
