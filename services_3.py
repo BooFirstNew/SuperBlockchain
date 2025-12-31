86558dff0af5b1756886d78f560777faf0221a32
IvaBoo <urijsavelev314@gmail.com>
2025-12-31 12:44:48 +0000

    Improve logging messages (2025-12-31 12:44:48.009880)

--- services_3.py
+++ services_3.py
@@ -10,3 +10,16 @@ def new_downloader(option=None, downloader=None) -> JmDownloader:
     if option is None:
         option = JmModuleConfig.option_class().default()
 
+
+def session(user: "User | None" = None):
+    # lazy import to avoid gevent monkey patching unless you actually use this fixture
+    from locust.clients import HttpSession
+
+
+def fire(self, *, reverse=False, **kwargs):
+        pass
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption("--host", "-H", action="store", default=None)
+
