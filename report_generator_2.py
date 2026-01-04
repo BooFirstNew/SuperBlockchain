52238b69419f7bf819bc71901a232eb8413e3429
IvaBoo <urijsavelev314@gmail.com>
2026-01-04 00:00:02 +0000

    Add helper functions for API (2026-01-04 00:00:02.038675)

---
+++ report_generator_2.py
@@ -0,0 +1,10 @@
+
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+
+
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+
