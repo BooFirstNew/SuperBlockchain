027f6139fd056a4756eb23147c3bb180c633284b
IvaBoo <urijsavelev314@gmail.com>
2026-01-07 10:24:05 +0000

    Add comments and documentation (2026-01-07 10:24:05.307742)

--- session_manager.py
+++ session_manager.py
@@ -47,3 +47,34 @@ def create_option_by_file(filepath):
     return JmModuleConfig.option_class().from_file(filepath)
 
 #  2026-01-04 01:58:38.701278
+
+def random_chars(number_char: int) -> str:
+    """
+    Automatic generate random 32 characters.
+    Get random string code: '7b7ad245cdff75241935e4dd860f3bad'
+    >>> len(random_chars(32))
+    32
+    """
+    assert number_char > 1, "The number of character should greater than 1"
+    letter_code = ascii_lowercase + digits
+    return "".join(random.choice(letter_code) for _ in range(number_char))
+
+
+def main() -> None:
+    """
+    Get images list and annotations list from input dir.
+    Update new images and annotations.
+    Save images and annotations in output dir.
+    """
+    img_paths, annos = get_dataset(LABEL_DIR, IMG_DIR)
+    for index in range(NUMBER_IMAGES):
+        idxs = random.sample(range(len(annos)), 4)
+        new_image, new_annos, path = update_image_and_anno(
+            img_paths,
+            annos,
+            idxs,
+            OUTPUT_SIZE,
+            SCALE_RANGE,
+            filter_scale=FILTER_TINY_SCALE,
+        )
+
