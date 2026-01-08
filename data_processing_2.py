4169782ab994af7880b80d2bd748fcef9c84320c
IvaBoo <urijsavelev314@gmail.com>
2026-01-08 12:48:57 +0000

    Optimize performance in utils (2026-01-08 12:48:57.612024)

--- data_processing_2.py
+++ data_processing_2.py
@@ -1,1 +1,12 @@
 #  2025-12-15 04:00:35.707227
+
+def create_option_by_file(filepath):
+    return JmModuleConfig.option_class().from_file(filepath)
+
+
+def create_option_by_str(text: str, mode=None):
+    if mode is None:
+        mode = PackerUtil.mode_yml
+    data = PackerUtil.unpack_by_str(text, mode)[0]
+    return JmModuleConfig.option_class().construct(data)
+
